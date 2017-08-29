19153e13a738a79dae02e2374f1f8b048372a115
汪中杰 <743549960@qq.com>
2017-08-29 18:03:51 +0800

    首页

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Slides } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild(Slides) slides: Slides;
+  
+    goToSlide() {
+      this.slides.slideTo(2, 500);
+    }
   constructor(public navCtrl: NavController) {
 
   }
